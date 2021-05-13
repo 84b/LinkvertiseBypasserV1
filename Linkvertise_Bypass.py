d731c4596699baf9368fcd9eb2bc819face90140
84b <68043225+84b@users.noreply.github.com>
2021-05-13 21:23:32 +0100

    Add files via upload

---
+++ Linkvertise_Bypass.py
@@ -0,0 +1,20 @@
+import requests
+import json
+import ctypes
+import os
+ctypes.windll.kernel32.SetConsoleTitleW("Linkvertise: Ad-Link Bypasser!")
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+def Main():
+    link = str(input("Enter your linkvertise link: "))
+    bypass=requests.get("https://bypass.bot.nu/bypass2?url={}".format(link))
+    success = bypass.json()['success']
+    if success == False:
+        cls()
+        print("Not a valid linkvertise link")
+    if success == True:
+        destination = bypass.json()['destination']
+        cls()
+        print(destination)
+if __name__ == '__main__':
+    Main()
